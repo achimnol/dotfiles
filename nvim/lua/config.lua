91684bdbb0366943523b4939c2acb5c23b8534ec
Joongi Kim <joongi@lablup.com>
2022-06-19 16:35:53 +0000

    nvim: Let nvim-tree open the dir view in the location of buffer file

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -3,10 +3,18 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = { "python", "c", "lua", "javascript", "rust" },
 }
 
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+}
+
+require('telescope').setup {
+}
 
 require('onedark').setup {
-    style = 'cool',
-    transparent = true,
+  style = 'cool',
+  transparent = true,
 }
 require('onedark').load()
