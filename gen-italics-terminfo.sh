18ec12d2d2f9edb0f74d501571e32696f15a19bc
Joongi Kim <joongi@lablup.com>
2022-06-29 05:29:49 +0000

    tmux: Improve terminfo generation

--- gen-italics-terminfo.sh
+++ gen-italics-terminfo.sh
@@ -14,31 +14,17 @@ fi
 # To use italics in "normal" terminals and tmux.
 # From tmux's FAQ section: https://github.com/ThomasAdam/tmux/blob/master/FAQ
 
-terms=( screen-256color xterm-256color )
-for term in "${terms[@]}"; do
-    $INFOCMP "$term" > $term.terminfo.bak  # backup existing terminfo
-    $INFOCMP "$term" | sed \
-      -e 's/%?%p1%t;3%/%?%p1%t;7%/' \
-      -e 's/smso=[^,]*,/smso=\\E[7m,/' \
-      -e 's/rmso=[^,]*,/rmso=\\E[27m,/' \
-      -e '$s/$/ sitm=\\E[3m, ritm=\\E[23m,/' > /tmp/$term.terminfo
-    $TIC /tmp/$term.terminfo
-    rm /tmp/$term.terminfo
-done
+# terms=( xterm-256color )
+# for term in "${terms[@]}"; do
+#     $INFOCMP "$term" > $term.terminfo.bak  # backup existing terminfo
+#     $INFOCMP "$term" | sed \
+#       -e 's/%?%p1%t;3%/%?%p1%t;7%/' \
+#       -e 's/smso=[^,]*,/smso=\\E[7m,/' \
+#       -e 's/rmso=[^,]*,/rmso=\\E[27m,/' \
+#       -e '$s/$/ sitm=\\E[3m, ritm=\\E[23m,/' > /tmp/$term.terminfo
+#     $TIC /tmp/$term.terminfo
+#     rm /tmp/$term.terminfo
+# done
 
-cat <<EOF > /tmp/tmux.terminfo
-tmux|tmux terminal multiplexer,
-	ritm=\E[23m, rmso=\E[27m, sitm=\E[3m, smso=\E[7m, Ms@,
-	use=xterm+tmux, use=screen,
-
-EOF
-$TIC -x /tmp/tmux.terminfo
-rm /tmp/tmux.terminfo
-
-cat <<EOF > /tmp/tmux-256color.terminfo
-tmux-256color|tmux with 256 colors,
-	use=xterm-256color, use=tmux,
-
-EOF
-$TIC -x /tmp/tmux-256color.terminfo
-rm /tmp/tmux-256color.terminfo
+# Combine xterm-256color and screen-256color to generate tmux-256color
+$TIC -x ./tmux-256color.terminfo
