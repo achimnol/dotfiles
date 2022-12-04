709e728ead4048ed58b7836e988dff3900e29ca3
Joongi Kim <joongi@lablup.com>
2022-12-04 10:14:20 +0000

    nvim: Update indent_blankline config

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -54,9 +54,9 @@ require('telescope').setup {
 }
 
 require('indent_blankline').setup {
-    -- for example, context is off by default, use this to turn it on
     show_current_context = true,
-    -- show_current_context_start = true,
+    show_current_context_start = false,
+    show_trailing_blankline_indent = false,
     use_treesitter = true,
 }
 
