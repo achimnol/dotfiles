0eee0183eeb369719ca532a395b3ea4a96f4631b
Joongi Kim <me@daybreaker.info>
2026-05-17 16:39:12 +0900

    scripts: Modernize using hombrew

--- scripts/build-ripgrep.sh
+++ scripts/build-ripgrep.sh
@@ -1,10 +1,2 @@
 #! /bin/bash
-
-git clone https://github.com/BurntSushi/ripgrep /tmp/ripgrep
-cd /tmp/ripgrep
-cargo build --release
-sudo cp ./target/release/rg /usr/local/bin/rg
-hash -r
-cd $HOME
-rm -rf /tmp/ripgrep
-rg --version
+brew install ripgrep
--- scripts/install-mosh.sh
+++ scripts/install-mosh.sh
@@ -1,11 +1,4 @@
 #! /bin/bash
-
-echo "===> Building latest mosh"
-git clone --branch 'mosh-1.4.0' https://github.com/mobile-shell/mosh /tmp/mosh
-cd /tmp/mosh
-sudo apt install -y libprotobuf-dev protobuf-compiler
-./autogen.sh
-./configure --prefix=/usr/local
-make && sudo make install
-cd
-rm -rf /tmp/mosh
+# Install a fork having more features: https://github.com/antonme/mosh
+brew tap antonme/tap
+brew install antonme/tap/mosh
